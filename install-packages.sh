ef219fca7a47234d23b6c6542e5cc39a3567181d
TrNgTien <ngoctien27122001@gmail.com>
2024-04-04 21:16:53 +0700

    adjust install script

--- install-packages.sh
+++ install-packages.sh
@@ -8,7 +8,17 @@ sudo apt-get install ca-certificates curl
 
 sudo apt-get install htop
 
-echo "[Installing] Docker"
+echo "[Installing] ZSH"
+
+sudo apt install zsh
+
+echo "[Installing] OH MY ZSH"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -Y
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+echo "[INSTALLING] Docker"
 sudo install -m 0755 -d /etc/apt/keyrings
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -21,15 +31,11 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "[Installing] ZSH"
-
-sudo apt install zsh
-
-echo "[Installing] OH MY ZSH"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -Y
-
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-echo "[INSTALLING] Docker"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -Y
+
+echo "[INSTALLING] nvm"
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+source ~/.profile
+
+nvm install v18.17.0
